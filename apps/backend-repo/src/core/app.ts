7843105165bc39904c725987b0962a45ee25c56a
fahrul muhammad <robot@fahruls-iMac-Pro.local>
2025-05-20 01:10:58 +0800

    trying firebase emulator but not done

--- apps/backend-repo/src/core/app.ts
+++ apps/backend-repo/src/core/app.ts
@@ -1,13 +1,15 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import userRoutes from "../routes/userRoutes";
 
 dotenv.config();
 
-const app = express();
+const app: any = express();
 const PORT = 5001;
 
 app.use(express.json());
+app.use(cors({ origin: true }));
 app.use(
   express.urlencoded({
     extended: true,
@@ -19,3 +21,5 @@ app.use(userRoutes);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
+
+export default app;
